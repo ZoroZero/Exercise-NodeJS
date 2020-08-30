c6ea7377c636c49411f60366397faa95e77b15f1
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-08-30 22:16:41 +0700

    Update http-edit.interceptor.ts

--- src/interceptors/http-edit.interceptor.ts
+++ src/interceptors/http-edit.interceptor.ts
@@ -4,12 +4,13 @@ import { map } from 'rxjs/operators';
 
 export interface EditResponse<T> {
   // data?: T;
+  id?: string;
   statusCode: HttpStatus;
 }
 
 @Injectable()
 export class EditInterceptor<T> implements NestInterceptor<T, EditResponse<T>> {
   intercept(context: ExecutionContext, next: CallHandler): Observable<EditResponse<T>> {
-    return next.handle().pipe(map(data => ({ statusCode: HttpStatus.OK })));
+    return next.handle().pipe(map(data => ({ id: data.id, statusCode: HttpStatus.OK })));
   }
 }
